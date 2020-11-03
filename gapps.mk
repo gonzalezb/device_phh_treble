04f40ff4fdb1af7c572ae39d37a8ce5bd0093343
Brenden Gonzalez <brendenrichardgonzalez@gmail.com>
2020-11-03 17:05:28 -0600

    Fix Google Hotword

--- gapps.mk
+++ gapps.mk
@@ -5,7 +5,7 @@ PRODUCT_COPY_FILES += \
 	device/phh/treble/empty-permission.xml:system/etc/permissions/com.google.android.camera.experimental2017.xml
 
 DEVICE_PACKAGE_OVERLAYS += device/phh/treble/overlay-gapps
-GAPPS_VARIANT := pico
+GAPPS_VARIANT := micro
 DONT_DEXPREOPT_PREBUILTS := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
@@ -13,6 +13,7 @@ GAPPS_FORCE_BROWSER_OVERRIDES := true
 GAPPS_FORCE_WEBVIEW_OVERRIDES := true
 PRODUCT_PACKAGES += \
        Chrome \
+       Search \
        CalculatorGoogle \
        PrebuiltDeskClockGoogle \
        CalendarGooglePrebuilt \
